7c0021684da3fb8e2e83455a25c7f47525c9b43c
Arkaj <arkajkesavms@gmail.com>
2023-01-24 00:33:50 +0530

    Singly-Linked-List
    1. Pseudo Code for SLL
    2.Created .C File for SLL
    3.Created Node for SLL

---
+++ Singly-Linked-List.c
@@ -0,0 +1,22 @@
+//header file in the C standard library that provides functions for input and output operations.
+#include<stdio.h>
+
+//create a node
+struct Sllnode
+{
+    //to store Data
+    int data;
+    //to store Next Node Address
+    struct Sllnode* next;
+};
+
+//Starting with the head node
+int create_sll_head()
+{
+    head = 
+}
+//The main function is an important part of a C program. It is the starting point of a program's execution and is where the program's instructions are executed.
+int  main()
+{
+    //Call functions and pass values to the sll function
+}
